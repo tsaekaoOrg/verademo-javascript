ca3905f7a2134d3cd2aa7ad4c61e034728167255
clydeshtino <clydeshtino@gmail.com>
2024-06-17 13:53:13 -0400

    Merge

---
+++ src-app/commands/ignoreCommand.js
@@ -0,0 +1,9 @@
+const BlabberCommand = require('./blabberCommand');
+
+class IgnoreCommand extends BlabberCommand {
+    constructor(cursor, username) {
+        logger.info("Ignore command");
+        super();
+        
+    }
+}
---
+++ src-app/controllers/homeController.js
@@ -0,0 +1,5 @@
+
+
+function home(req) {
+    
+}

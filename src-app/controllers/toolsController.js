f7e0d893085d8ce485cb420fe30d932dfa59dc3a
kevliu43 <kevliu43@gmail.com>
2024-06-18 11:54:53 -0400

    tools fixes

--- src-app/controllers/toolsController.js
+++ src-app/controllers/toolsController.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const e = require('express');
 var express = require('express');
 const { loggers } = require('winston');
 // var tools = require('tools.js');
@@ -14,26 +15,43 @@ function showTools(req, res) {
     res.render('tools.hbs', {});
 }
 // Performs actions on tools page
-function processTools(req, res) {
-    var host = req.body.host;
-    const fortunefile = req.body.fortunefile;
-   // file = fortunefile ? fortune(fortunefile) : "";
-    req.host = host;
-    ping = host ? ping(host) : "";
-    console.log( "Kevin",ping);
+async function processTools(req, res) {
+    try {
+        var host = req.body.host;
+        const fortunefile = req.body.fortunefile;
+        console.log(host);
+        // file = fortunefile ? fortune(fortunefile) : "";
+        // req.host = host;
+        // var pingResult = host ? ping(host) : "";
+        if (host) {
+            ping(host, function(ping) {
+                return res.render('tools', {host, ping});   
+            })
+        } else if (fortunefile) {
+            fortune(file, function(result) {
+                return res.render('tools', {result});   
+            })
+        } else {
+            return res.render('tools');
+        }
 
-    return res.render('tools', {host : host, /*file: fil*/ ping : ping});   
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
 // Pings selected host based on user input, then outputs the results
-function ping(host) {
+async function ping(host, callback) {
     let output = "";
     console.log("Pinging " + host);
     try {
-        const pingProcess = spawn('ping', ['-c', '1', host]);
+        var pingProcess = spawn('ping', ['-c', '1', host]);
         pingProcess.stdout.on('data', (data) => {
             output = data.toString();
             console.log(output);
             console.log("Exit code: " + pingProcess.exitCode);
+            callback(output);
         });
     } catch (err) {
         console.log("Error occured during ping: ", err);

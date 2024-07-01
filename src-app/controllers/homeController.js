2741ad4c081677c5ed915cec98348cca5e136729
Stuart Sessions <ssessions@veracode.com>
2024-07-01 16:10:56 -0400

    CWE-918

--- src-app/controllers/homeController.js
+++ src-app/controllers/homeController.js
@@ -1,12 +1,19 @@
+const axios = require('axios')
+const userController = require('./userController')
+
 async function renderGet(req, res) {
     try {
+        console.log('check if user is logged in')
         if (req.session.username) {
-            return res.redirect('/feed')
+            console.log('user logged in, redirecting to feed')
+            host_ip = req.get('host').split(':')[0]
+            axios.request({
+                method: 'get',
+                url: 'http://' + host_ip + ':' + req.socket.localPort + '/feed',
+                maxContentLength: 5000,
+            })
         }
-
-        res.locals.username = ""
-
-        return res.render('login', {})
+        return userController.showLogin(req, res)
     }
     catch (err) {
         console.log(err);

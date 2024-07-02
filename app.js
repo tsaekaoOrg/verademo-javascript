d0bdff4e25fc4bc6baaa03d19a205ae4fc472970
kevliu43 <kevliu43@gmail.com>
2024-07-02 16:47:05 -0400

    fixed variables

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'resources')));
-app.use(session({secret: "secret"}));
+app.use(session({secret: "secret", saveUninitialized: true, resave: true}));
 
 app.use('/', indexRouter);
 
